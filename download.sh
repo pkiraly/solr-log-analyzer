b74cf918951ebcdf4181b33c0728058077678d9e
Kiraly Peter <peter.kiraly@kb.nl>
2014-07-18 14:46:56 +0200

    PATH -> SOLR_LOG_PATH

--- download.sh
+++ download.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
-USER=euadmin
-HOST=-prd-vh.europeana.vancis.nl
-PATH=/solr/logs
+. ./config.sh
 for date in '07-07' '07-08' '07-09' '07-10' '07-11' '07-12' '07-13' '07-14' '07-15' '07-16' '07-17'
 do
 	for server in 10 11 12 13
 	do
 		DOMAIN=solr$server
 		FILE=catalina.2014-$date.log
-		rsync -avPz $USER@$DOMAIN$HOST:$PATH/$FILE logfiles/$DOMAIN-$FILE
+		rsync -avPz $USER@$DOMAIN$HOST:$SOLR_LOG_PATH/$FILE logfiles/$DOMAIN-$FILE
 	done
 done
